2062a7b68ef48a42c0a0d160f22f23bbd291a61a
boyfromthewell <tnsdyd10@naver.com>
2021-09-16 20:37:32 +0900

    React

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -7,18 +7,30 @@ function App() {
   const increment = () => {
     setCount(count + 1);
   };
-  const [buttonName, setButtonName]=useState('클릭');
+  const [buttonName, setButtonName] = useState("클릭");
 
-  const clickButton=()=>{
-    setButtonName('click');
+  const clickButton = () => {
+    setButtonName("click");
   };
+  const [condition, setCondition] = useState(false);
+
+  const toggle = () => setCondition(!condition);
+
+  useEffect(() => {
+    console.log(condition);
+  }, [condition]);
+
+  const renderCondition = condition ? "True" : "False";
+
   return (
     <div className="App">
       <h1>soon</h1>
       <Counter click="click1" />
-      <Counter click={buttonName}/>
+      <Counter click={buttonName} />
       <button onClick={increment}>click {count}</button>
       <button onClick={clickButton}>Click</button>
+      <div>{renderCondition}</div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
