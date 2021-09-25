2462f8f298d56b80c1819c0835f5ee521de6a473
boyfromthewell <tnsdyd10@naver.com>
2021-09-25 17:30:27 +0900

    React JS

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,56 +1,26 @@
 import React, { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-import MovieForm from "./components/MovieForm";
 import Navbar from "./components/Navbar";
-import Users from "./pages/Users";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { __RouterContext } from "react-router";
-
+import routers from "./routers";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(
-      movies.filter((movie) => {
-        return movie.id !== id;
-      })
-    );
-  };
-
-  const renderMovies = movies.length
-    ? movies.map((movie) => {
-        return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
-      })
-    : "추가된 영화가 없습니다.";
-
-  const addMovie = (movie) => {
-    setMovies([...movies, movie]);
-  };
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="container">
-      <Switch>
-      <Route path="/movies">
-      <h1>Movie List</h1>
-      <MovieForm addMovie={addMovie} />
-      {renderMovies}
-      </Route>
-      <Route path="/" exact>
-        <h1>Home</h1>
-      </Route>
-      <Route path="/users">
-        <Users/>
-      </Route>
-      </Switch>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            {routers.map((route) => {
+              return (
+                <Route key={route.path} path={route.path} exact>
+                  <route.component />
+                </Route>
+              );
+            })}
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
